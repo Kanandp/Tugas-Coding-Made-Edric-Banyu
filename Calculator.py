ee37ff77501563aff93dc1cd5896f2fc0bb1f37d
kanand <madedayanand@gmail.com>
2024-05-29 19:28:45 +0700

    2nd commit

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,6 @@
 print("Calculator By Made Edric Banyu")
-input1= int(input("type first number:"))
-input2= int(input("type second number:"))
+input1= int(input("type first number: "))
+input2= int(input("type second number: "))
 
 add = input1 + input2
 sub = input1 - input2
@@ -8,7 +8,7 @@ div = input1 / input2
 mul = input1 * input2
 
 print("Answers")
-print("Addition=",add)
-print("Subtraction",sub)
-print("Divide=",div)
-print("Multiply=",mul)
+print("Addition= ",add)
+print("Subtraction= ",sub)
+print("Divide= ",div)
+print("Multiply= ",mul)
